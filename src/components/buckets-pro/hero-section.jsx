90f0924a37d31a73b58b2c4c3c2201b69b852224
matthewdavid <mattdavid37@gmail.com>
2024-08-05 10:55:42 -0400

    Trail < Trial

--- src/components/buckets-pro/hero-section.jsx
+++ src/components/buckets-pro/hero-section.jsx
@@ -1,20 +1,18 @@
-import { Button } from "@components/common"
+import { Button } from '@components/common';
 const HeroSection = () => {
-    return (
-        <section className="relative h-fit bg-hero-buckets-pro bg-cover bg-center">
-            <div className="absolute inset-0 bg-[#1E2764] opacity-50"></div>
-            <div className="container relative m-auto pb-16 pt-56 md:mt-12">
-                <p className="text-center font-fontOpen text-heading6 text-white md:text-left">
-                    Products
-                </p>
-                <h2 className="text-center font-fontOpen text-headingLargeMobile font-bold text-white md:text-left md:text-headingLarge">
-                    Buckets Pro+
-                </h2>
-                <div className="flex justify-center md:justify-start">
-                    <Button>Start Free Trail</Button>
-                </div>
-            </div>
-        </section>
-    )
-}
-export default HeroSection
+  return (
+    <section className="relative h-fit bg-hero-buckets-pro bg-cover bg-center">
+      <div className="absolute inset-0 bg-[#1E2764] opacity-50"></div>
+      <div className="container relative m-auto pb-16 pt-56 md:mt-12">
+        <p className="text-center font-fontOpen text-heading6 text-white md:text-left">Products</p>
+        <h2 className="text-center font-fontOpen text-headingLargeMobile font-bold text-white md:text-left md:text-headingLarge">
+          Buckets Pro+
+        </h2>
+        <div className="flex justify-center md:justify-start">
+          <Button>Start Free Trial</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default HeroSection;
--- src/components/common/email-input.jsx
+++ src/components/common/email-input.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
 const EmailInput = ({ className }) => {
-    return (
-        <form className={className}>
-            <div className="relative flex justify-center md:justify-start">
-                <input
-                    type="email"
-                    className="h-[3.125rem] w-4/5 rounded-l-[1.563rem] border-b border-l border-t border-[#928F8F] ring-[#008CFF] focus:outline-none focus:ring-1 lg:w-[18rem] lg:pl-4"
-                    placeholder="Enter your email"
-                />
-                <button className="rounded-e-[1.563rem] bg-[#008CFF] font-fontOpen text-sm font-medium text-white md:p-2 md:px-5 md:text-[1.063rem]">
-                    START FREE TRAIL
-                </button>
-            </div>
-        </form>
-    )
-}
-export default EmailInput
+  return (
+    <form className={className}>
+      <div className="relative flex justify-center md:justify-start">
+        <input
+          type="email"
+          className="h-[3.125rem] w-4/5 rounded-l-[1.563rem] border-b border-l border-t border-[#928F8F] ring-[#008CFF] focus:outline-none focus:ring-1 lg:w-[18rem] lg:pl-4"
+          placeholder="Enter your email"
+        />
+        <button className="rounded-e-[1.563rem] bg-[#008CFF] font-fontOpen text-sm font-medium text-white md:p-2 md:px-5 md:text-[1.063rem]">
+          START FREE TRIAL
+        </button>
+      </div>
+    </form>
+  );
+};
+export default EmailInput;
